3eba5e29f8e172e1547b50eabece8dbfbe60f129
Cris117 <95005685+Criss117@users.noreply.github.com>
2026-07-13 15:32:12 -0500

    feat: add GestureHandlerRootView wrapper and Text component
    Wrap Integrations provider in GestureHandlerRootView for gesture
    support. Add reusable Text component with heroui-native cn() class
    merging and default font-normal styling.

--- src/integrations/index.tsx
+++ src/integrations/index.tsx
@@ -1,10 +1,13 @@
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { FontsProvider } from "./fonts";
 import { HeroUIProvider } from "./heroui";
 
 export function Integrations({ children }: { children: React.ReactNode }) {
   return (
-    <FontsProvider>
-      <HeroUIProvider>{children}</HeroUIProvider>;
-    </FontsProvider>
+    <GestureHandlerRootView>
+      <FontsProvider>
+        <HeroUIProvider>{children}</HeroUIProvider>;
+      </FontsProvider>
+    </GestureHandlerRootView>
   );
 }
